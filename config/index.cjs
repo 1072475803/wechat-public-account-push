791682f80066e78054a4973b71e3918e3ce287bc
1072475803 <112608936+1072475803@users.noreply.github.com>
2023-04-05 16:41:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,30 +13,27 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '451418038de0d01aab1d85986308beb8',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江苏',
+  CITY: '苏州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '岭宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouUfm5kMf4_o43V0Cl7nMmIFyyTs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2w4bQOhiZBCzsd2dn9KzqWJHRT9E320bsw1Vce7NIAQ',
+      useTemplateId: '	WsDbjgcDbMV-MPSQLmg1pVZqGmbGD1XHgL1ScAFB7OY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '1-23',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '岭宝', year: '1998', date: '01-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '生日', name: '岭宝', year: '1998', date: '02-19',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -64,4 +61,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
