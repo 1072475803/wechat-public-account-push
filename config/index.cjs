5f225c8a0956ec5e9da0e44903afbe3f9356d5f1
1072475803 <112608936+1072475803@users.noreply.github.com>
2023-04-11 09:31:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '岭宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ouUfm5qmnILcNFeE2V2qt0lmClCk',
+      id: 'ouUfm5kMf4_o43V0Cl7nMmIFyyTs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'WsDbjgcDbMV-MPSQLmg1pVZqGmbGD1XHgL1ScAFB7OY',
+      useTemplateId: 'azxAHXKFQf3SV1Ei1rh8QFtfrNyyJn4u8KFSZwH0sQs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-23',
       festivals: [
